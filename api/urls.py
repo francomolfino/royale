9d15bec7f71885ddaa335078a50f0d65bcd3043e
francomolfino <francomolfino11@gmail.com>
2018-10-14 20:18:20 -0300

    User auth

---
+++ api/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url, include
+
+urlpatterns = [
+    url('rest-auth/', include('rest_auth.urls')),
+    url('rest-auth/registration/', include('rest_auth.registration.urls')),
+]
